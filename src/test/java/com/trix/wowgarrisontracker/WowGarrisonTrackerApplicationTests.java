73bb115c1c2ff25a83c0703f5b38de13aa34209c
Jakub Pietrzyk <Jakub.Olszowski@wp.pl>
2020-12-20 14:19:01 +0100

    Project structure

---
+++ src/test/java/com/trix/wowgarrisontracker/WowGarrisonTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trix.wowgarrisontracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowGarrisonTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
