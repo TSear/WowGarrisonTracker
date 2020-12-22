86c228833cbed4f3809ccc8b6390a1abf2619019
Jakub Pietrzyk <Jakub.Olszowski@wp.pl>
2020-12-22 17:33:09 +0100

    Created converters, edited testing controller and pojos

--- src/main/java/com/trix/wowgarrisontracker/pojos/AccountCharacterPojo.java
+++ src/main/java/com/trix/wowgarrisontracker/pojos/AccountCharacterPojo.java
@@ -2,9 +2,18 @@ package com.trix.wowgarrisontracker.pojos;
 
 import java.util.Set;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@Setter
+@Getter
+@Data
 public class AccountCharacterPojo {
     
-
     private Long id;
     private String characterName;
     private Long accountId;
