b67cff63f00485d1bc6f03a5429f9346e00879b9
Jakub Pietrzyk <Jakub.Olszowski@wp.pl>
2021-03-03 15:07:11 +0100

    .

--- src/main/java/com/trix/wowgarrisontracker/model/AccountCharacter.java
+++ src/main/java/com/trix/wowgarrisontracker/model/AccountCharacter.java
@@ -2,14 +2,7 @@ package com.trix.wowgarrisontracker.model;
 
 import java.util.Set;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,6 +13,7 @@ import lombok.ToString;
 @NoArgsConstructor
 @Setter
 @Getter
+
 @Entity
 public class AccountCharacter {
 
